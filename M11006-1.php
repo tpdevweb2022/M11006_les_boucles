c8a66073ea530507a78530b332eaed2e79d16607
anthonymerlier <webmaster@consor.fr>
2022-04-12 07:29:49 +0200

    Add consignes && add index

---
+++ M11006-1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <div>
+        <a href="/">Retour à l'index</a>
+    </div>
+    <div>
+        <p><?= 'Créer une boucle avec la méthode while, générant dans chaque affichage "Ceci est la lettre "suivi d\'un contenu dynamique allant de la lettre "a" à la lettre "g"'
+
+            // VOS SCRIPTS CI-DESSOUS
+
+
+            ?></p>
+    </div>
+</body>
+
+</html>
---
+++ M11006-2.php
@@ -0,0 +1,31 @@
+<?php
+
+$cars = [
+    "Audi"      => "R8",
+    "BMW"   => "M3",
+    "Bugatti" => "Veyron",
+    "Ferrari" => "F430",
+    "Porsche" => "Cayman",
+];
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Véhicules</title>
+</head>
+
+<body>
+    <div>
+        <a href="/">Retour à l'index</a>
+    </div>
+    <ul>
+        <li></li>
+    </ul>
+</body>
+
+</html>
